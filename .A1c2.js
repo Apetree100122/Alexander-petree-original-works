73680e425ed8d33cc699996114382f04106534aa
Alexander Petree <apetree1001@email.phoenix.edu>
2024-10-16 05:22:12 -0700

    Update and rename A1c2 to .A1c2.js

---
+++ .A1c2.js
@@ -0,0 +1,32 @@
+
+{  
+"id": "/subscriptions/33af1e61-072c-48f1-bd41-b60da094e056/resourceGroups/Locker/providers/Microsoft.KeyVault/vaults/A1c2",
+"name": "A1c2",  
+"type": "Microsoft.KeyVault/vaults", 
+"location": "eastus", 
+"tags": { "83968350": "1000", "alexander@alexander.omnimicrosoft.com": "1000",  "038811634763": "2","48E2E74D21E4D74F": "1000","1681196997174": "2","1681829024810":"102" }, 
+"properties": {"sku": {"family": "A","name":"Premium" },
+   "tenantId": "47ea4cec-fe0f-48f5-9a09-a2f957278304", 
+"networkAcls"[],            
+"virtualNetworkRules": [] 
+              },"accessPolicies": [{"tenantId": "47ea4cec-fe0f-48f5-9a09-a2f957278304", "objectId": "0e3d0b02-31be-4bee-89e2-20f40df4d4fc",
+"permissions":{"keys": 
+        ["Get","List",
+         "Update","Create",
+         "Import","Delete",
+         "Recover","Backup", 
+         "RESTORE",        
+         "enabledForDeployment": true, 
+         "enabledForDiskEncryption": true,        
+         "enabledForTemplateDeployment": true,  
+         "enableSoftDelete": true,        
+         "softDeleteRetentionInDays": 30,
+         "enableRbacAuthorization": false,
+         "vaultUri": "https://a1c2.vault.azure.net"/"provisioningState": "Succeeded"
+         }};
+
+                   // copyright 2022-12-26 GNU 3 
+                // Alexander petree<apetree1001@email.phoenix.edu 
+
+
+                
